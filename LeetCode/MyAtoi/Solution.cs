b07cb70510e7446d777c04e33bea431d31c0fa4b
yongliang_li <yongliang_li@dell.com>
2017-06-02 14:10:15 -0500

    Add MyAtoi and Valid Parentheses projects

---
+++ LeetCode/MyAtoi/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAtoi
+{
+    public class Solution
+    {
+        /*
+         How many ways to check int overflow?
+         1. use check keyword to force throw OverflowException
+         2. using long variable to save the value, then compare with int.MaxValue || int.MinValue
+         3. compare with int.MaxValue ret > int.MaxValue/10 || (ret == int.MaxValue && n>7)
+             
+             */
+        public int MyAtoi(string str)
+        {
+            var initialized = false;
+            var sign = 1;
+            var ret = 0;
+            foreach (var c in str)
+            {
+                if (initialized == false && char.IsWhiteSpace(c)) continue;
+                if (initialized == false && (c == '+' || c == '-'))
+                {
+                    initialized = true;
+                    if (c == '-') sign = -1;
+                    continue;
+                }
+                if (char.IsNumber(c) == false) return ret;
+
+                if (initialized == false) initialized = true;
+                var n = (int)char.GetNumericValue(c);
+                try
+                {
+                    checked
+                    {
+                        ret = ret * 10 + n * sign;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    return sign > 0 ? int.MaxValue : int.MinValue;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ LeetCode/ValidParentheses/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    public class Solution
+    {
+        /*
+         1. Dictionary initialize
+         2. Stack Push, Pop, Count             
+             */
+
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            var hs = new Dictionary<char, char>(){
+                {'}','{'},
+                { ']','['},
+                { ')',')'}
+            };
+
+            foreach (var c in s)
+            {
+                if (hs.Keys.Contains(c))
+                {
+                    if (hs[c] != stack.Pop()) return false;
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            return stack.Count == 0;
+
+        }
+    }
+}
